c8f5f980f9c1cf67f985093b3cb713ed7143decf
Saurabh <you@example.com>
2023-03-25 13:03:58 +0530

    Added Routing to StudentComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentcomponentComponent } from './studentcomponent/studentcomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'student',component:StudentcomponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
